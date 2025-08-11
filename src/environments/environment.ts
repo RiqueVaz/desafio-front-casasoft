93bb176d21eba1f332c66cba55f4443cf3845bec
RiqueVaz <henriquevazctba@hotmail.com>
2025-08-11 07:26:26 -0300

    Configurações de proxy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiAuth: 'https://casasoftchamado.casasoftsig.net.br/autenticacao',
-  apiChamado: 'https://casasoftchamado.casasoftsig.net.br/chamado',
-  signalrHub: 'https://casasoftchamado.casasoftsig.net.br/hub'
+  apiAuth: '/autenticacao',
+  apiChamado: '/chamado',
+  signalrHub: '/hub'
 };
